d554e79ecc812265d3ebe0f8a69baca022017499
RainOfPhone <792209198@qq.com>
2018-06-22 02:19:55 +0800

    实现了搜索博物馆的功能

--- pages/museumIndex/museumIndex.js
+++ pages/museumIndex/museumIndex.js
@@ -2,16 +2,17 @@ var util = require('../../utils/util.js')
 
 Page({
   data: {
-    museumList:[],
+    searchValue: 'museum:*',
+    museumList: []
   },
-  getMuseum: function () {
+  getMuseum: function() {
     var that = this;
     var url = '/select';
     var data = {
-      fl:'id,museumName,image_urls',
-      starts:0,
-      rows:85,
-      q: 'museum:*'
+      fl: 'id,museumName,image_urls',
+      starts: 0,
+      rows: 85,
+      q: this.data.searchValue
     };
     util.http('GET', url, data, (response) => {
       if (response.errMsg) {
@@ -24,23 +25,26 @@ Page({
       }
     })
   },
-  changeMuseum: function(e){
-    if (e.detail.value == ''){
-      this.setData({
-        searchValue: e.detail.value
-      })
+  bindKeyInput: function(e) {
+    var q = '', final = ''
+    if (e.detail.value === '' || e.detail.value == undefined){
+      q = '*'
     }else{
-      this.setData({
-        searchValue: 'museum:*'
-      })
+      q = e.detail.value
     }
+    console.log("e值", e.detail.value)
+    final = "museum:"+q
+    this.setData({
+      searchValue: final
+    })
+    this.getMuseum();
   },
-  setNavigationBarTitleText: function () {
+  setNavigationBarTitleText: function() {
     wx.setNavigationBarTitle({
       title: "· 寻墨丹青 ·"
     })
   },
-  onReady: function () {
+  onReady: function() {
     this.setNavigationBarTitleText();
     this.getMuseum();
   }
